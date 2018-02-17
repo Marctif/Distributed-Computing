25606319becb635d283f718e4e2f7264ace7098c
Ishan <contact@ishansharma.com>
2018-02-17 16:20:10 -0600

    Got the nodes to talk to each other

--- src/HomeworkDriver.java
+++ src/HomeworkDriver.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.PriorityQueue;
 
@@ -17,7 +18,7 @@ public class HomeworkDriver {
         String hostname = "";
         Config ownConfig = null;
         HashMap<String, Config> nodesByHostname;
-        HashMap<String, Config> nodesByID;
+        HashMap<String, Config> nodesByID = null;
         try {
             // get own hostname. This is required to find which node this is
             hostname = InetAddress.getLocalHost().getHostName();
@@ -44,31 +45,39 @@ public class HomeworkDriver {
 
         int ownUID = 0;
         int ownPort = 0;
+        String ownHostname = "";
+        ArrayList<Integer> ownNeighbors = null;
         try {
             ownUID = ownConfig.getUID();
             ownPort = ownConfig.getPort();
+            ownHostname = ownConfig.getHostname() + ".utdallas.edu"; // hostname only contains dcXX part
+            ownNeighbors = ownConfig.getNeighbors();
         } catch (NullPointerException e) {
             System.err.println(String.format("Error! Could not find hostname in config file for %s.", hostname));
         }
 
         System.out.println(String.format("Launching with UID %d, listening at port %d", ownUID, ownPort));
 
-//        int ownPort = Integer.parseInt(args[1]);
-//        int targetPort = Integer.parseInt(args[2]);
-//
-//        NetworkManager network = new NetworkManager(hostName,ownPort);
-//        network.startServer();
-//
-//        boolean n1Ready = network.nodeReady(hostName, targetPort); //While loops
-//        if(n1Ready)
-//            System.out.println("Child at " + hostName + " " + targetPort + " is ready for messages");
-//
-//        // String rsp2 = network.sendTestMessage("Test hello #2",hostName, targetPort);
-//        while(true){
-//            PriorityQueue<String> queue = network.getMessageQueue();
-//            if(queue.peek() != null){
-//                System.out.println(queue.poll());
-//            }
-//        }
+        // for now, let's just get two nodes to talk to each other
+        // int targetPort = Integer.parseInt(args[2]);
+        Config targetNodeConfig = nodesByID.get(Integer.toString(ownNeighbors.get(0)));
+        int targetNodeUID = targetNodeConfig.getUID();
+        int targetNodePort = targetNodeConfig.getPort();
+        String targetNodeHostname = targetNodeConfig.getHostname();
+
+        NetworkManager network = new NetworkManager(ownHostname, ownPort);
+        network.startServer();
+
+        boolean n1Ready = network.nodeReady(targetNodeHostname, targetNodePort); //While loops
+        if (n1Ready)
+            System.out.println("Child at " + targetNodeHostname + " " + targetNodePort + " is ready for messages");
+
+        // String rsp2 = network.sendTestMessage("Test hello #2",hostName, targetPort);
+        while (true) {
+            PriorityQueue<String> queue = network.getMessageQueue();
+            if (queue.peek() != null) {
+                System.out.println(queue.poll());
+            }
+        }
     }
 }
